bf8a0d6f6fb486f03a5279fbd7cd500132dac452
Elias Ablan <eliasablan93@gmail.com>
2023-10-17 15:43:56 -0400

    Footer mods

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,14 @@
-import styles from "../styles/Footer.module.css";
+import styles from '../styles/Footer.module.css';
 
 const Footer = () => {
   return (
     <p className={`mt-10 text-xs text-center ${styles.footer}`}>
-      This application was created by{" "}
+      This application was created by{' '}
       <a href="https://github.com/eliasablan" target="_blank">
-        <span className="text-white hover:underline">Elias Ablan</span>
+        <span className="font-medium underline hover:no-underline">
+          Elias Ablan
+        </span>
       </a>
-      {/* . Data for this application is sourced from{" "} */}
-      {/* <a href="https://swapi.dev/" target="_blank"> */}
-      {/* <span className="text-white hover:underline">SWAPI</span> */}
-      {/* </a> */}.
     </p>
   );
 };
