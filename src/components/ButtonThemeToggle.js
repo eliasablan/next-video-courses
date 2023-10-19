13725bb7ef1fa1f24ce0c7eaddeef642a99c0a69
Elias Ablan <eliasablan93@gmail.com>
2023-10-18 20:45:32 -0400

    edit dark or light theme button styling

--- src/components/ButtonThemeToggle.js
+++ src/components/ButtonThemeToggle.js
@@ -1,23 +1,21 @@
-"use client";
+'use client';
+import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
+import { useTheme } from 'next-themes';
 
-import * as React from "react";
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
-import { useTheme } from "next-themes";
-
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+} from '@/components/ui/dropdown-menu';
 
 export function ModeDropdown() {
   const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className="m-2" asChild>
+      <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
@@ -25,13 +23,13 @@ export function ModeDropdown() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
+        <DropdownMenuItem onClick={() => setTheme('light')}>
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
+        <DropdownMenuItem onClick={() => setTheme('dark')}>
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
+        <DropdownMenuItem onClick={() => setTheme('system')}>
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
@@ -46,7 +44,7 @@ export function ModeToggle() {
 
   return (
     <Button
-      onClick={() => setTheme(theme == "dark" ? "light" : "dark")}
+      onClick={() => setTheme(theme == 'dark' ? 'light' : 'dark')}
       variant="secondary"
       size="icon"
     >
